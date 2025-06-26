ec9298b89b8becc657dc060f0f06d83dd1173ebb
Tifa Chen <tifa.chen@technexion.com>
2025-06-26 00:55:52 -0700

    script: Add hdmi panel support

--- technexion_jetpack_download.sh
+++ technexion_jetpack_download.sh
@@ -181,6 +181,7 @@ mv_needed_files_for_demo_image(){
 		sudo cp -rp ${SRC_DIR}/${KERNEL_OUT}/kernel-devicetree/generic-dts/dtbs/*tevs* Linux_for_Tegra/rootfs/boot/
 		sudo cp -rp ${SRC_DIR}/${KERNEL_OUT}/kernel-devicetree/generic-dts/dtbs/*vls* Linux_for_Tegra/rootfs/boot/
 		sudo cp -rp ${SRC_DIR}/${KERNEL_OUT}/kernel-devicetree/generic-dts/dtbs/tegra234-p3768-0000+p3767-000*-nv.dtb Linux_for_Tegra/rootfs/boot/
+		sudo cp -rp ${SRC_DIR}/${KERNEL_OUT}/kernel-devicetree/generic-dts/dtbs/*hdmi* Linux_for_Tegra/rootfs/boot/
 	fi
 
 	# copy pinmux file
