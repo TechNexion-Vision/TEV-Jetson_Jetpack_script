d1d0265f371b3ffabfecfe332da8ad97f2984333
Tifa Chen <tifa.chen@technexion.com>
2026-05-15 16:21:56 +0800

    script: update tools path

--- technexion_jetpack_download.sh
+++ technexion_jetpack_download.sh
@@ -310,11 +310,11 @@ mv_needed_files_for_demo_image(){
 
 	# copy script files
 	cd ${CUR_DIR}
-	sudo cp -rp set_config.sh Linux_for_Tegra/rootfs/home/ubuntu/.
+	sudo cp -rp tools/set_config.sh Linux_for_Tegra/rootfs/home/ubuntu/.
 	if [[ ${board_conf} == "jetson-orin-nano-devkit" ]]; then
-		sudo cp -rp stream_gmsl2_8cam_w_ext_fsync_p15_orin_split.sh Linux_for_Tegra/rootfs/home/ubuntu/.
+		sudo cp -rp tools/stream_gmsl2_8cam_w_ext_fsync_p15_orin_split.sh Linux_for_Tegra/rootfs/home/ubuntu/.
 	elif [[ ${board_conf} == "jetson-agx-orin-devkit" ]]; then
-		sudo cp -rp stream_gmsl2_8cam_w_ext_fsync_p15_agx_split.sh Linux_for_Tegra/rootfs/home/ubuntu/.
+		sudo cp -rp tools/stream_gmsl2_8cam_w_ext_fsync_p15_agx_split.sh Linux_for_Tegra/rootfs/home/ubuntu/.
 	fi
 
 	# install vizionviewer in rootfs
