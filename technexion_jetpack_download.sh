85b4614cc74c6c8a6d57e7c434ab2a2fdce536b7
Tifa Chen <tifa.chen@technexion.com>
2026-06-10 15:49:23 +0800

    script: add tag r36.4.ga_tek7000

--- technexion_jetpack_download.sh
+++ technexion_jetpack_download.sh
@@ -7,7 +7,7 @@ BRANCH="tn_l4t-r36.4.ga_kernel-5.15"
 # device tree branch
 BRANCH_DT="tn_l4t-r36.4.3.ga_kernel-5.15"
 
-VALID_TAG=("r36.4.ga")
+VALID_TAG=("r36.4.ga" "r36.4.ga_tek7000")
 VALID_JP=("jp62" "jp621")
 VALID_BOOT=("tevs-dual" "vls" "vls-gm2" "vls-gm2-fsync" "vls-gm2-tunnel" "vls-gm2-tunnel-fsync" "vls-gm2-fsync-external")
 VALID_BOOT_TN=("vls-gm2-8cam" "vls-gm2-4cam")
@@ -254,6 +254,10 @@ mv_needed_files_for_demo_image(){
 				VV_FILE="vizionviewer-25.06.1-linuxarm64.tar.xz"
 				VV_URL="https://download.technexion.com/vizionviewer/linux_arm64/${VV_FILE}"
 				;;
+			r36.4.ga_tek7000)
+				VV_FILE="vizionviewer-26.05.1-linuxarm64.tar.xz"
+				VV_URL="https://download.technexion.com/vizionviewer/linux_arm64/${VV_FILE}"
+				;;
 			*)
 				# Let VV_URL empty, cause error when try to download
 				;;
@@ -424,7 +428,7 @@ usage() {
 	echo "  JETSON-ORIN-NANO-EVK| JETSON-AGX-ORIN-EVK" 1>&2
 	echo "" 1>&2
 	echo "-t: tag for sync code:" 1>&2
-	echo "${VALID_TAG}" 1>&2
+	echo "${VALID_TAG[@]}" 1>&2
 	echo "" 1>&2
 	echo "-v: jetpack version for sync code:" 1>&2
 	echo "${VALID_JP[@]}" 1>&2
